4404d3105a8386d958bc89683384e6d07f2d0c30
muhammad ramadhan <mr.maulanaasyarief@gmail.com>
2023-04-13 06:15:36 +0700

    fix so split_pdf can get file name automaticaly

--- split_pdf.py
+++ split_pdf.py
@@ -2,6 +2,8 @@ import os
 from pikepdf import Pdf
 from pprint import pprint
 import pickle as pkl
+import glob
+import os
 
 def set_file2pages():
     # arr = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1]
@@ -26,44 +28,51 @@ def set_file2pages():
 file2pages, unit = set_file2pages()
 
 # the target PDF document to split
-filename = "result/pdf/Final Invoice April 23.pdf"
+# filename = "result/pdf/Final Invoice April 23.pdf"
 
-# load the PDF file
-pdf = Pdf.open(filename)
+# get all csv files
+path =  os.path.dirname(os.path.realpath(__file__))
+folder_name = "result/pdf/"
+read_files = glob.glob(path+"/" + folder_name + "/*.pdf")
 
-# make the new splitted PDF files
-new_pdf_files = [ Pdf.new() for i in file2pages ]
-# the current pdf file index
-new_pdf_index = 0
+# loop all pdf files
+for read_file in read_files:
+    # load the PDF file
+    pdf = Pdf.open(read_file)
 
-# iterate over all PDF pages
-for n, page in enumerate(pdf.pages):
-    if n in list(range(*file2pages[new_pdf_index])):
-        # add the `n` page to the `new_pdf_index` file
-        new_pdf_files[new_pdf_index].pages.append(page)
-        print(f"[*] Assigning Page {n} to the file {unit[new_pdf_index]}")
-    else:
-        # make a unique filename based on original file name plus the index
-        name, ext = os.path.splitext(filename)
-        names = name.split("/")
-        names.pop()
-        name = "/".join(names)
-        output_filename = f"{name}/{unit[new_pdf_index]}.pdf"
+    # make the new splitted PDF files
+    new_pdf_files = [ Pdf.new() for i in file2pages ]
+    # the current pdf file index
+    new_pdf_index = 0
 
-        # save the PDF file
-        new_pdf_files[new_pdf_index].save(output_filename)
-        print(f"[+] File: {output_filename} saved.")
-        # go to the next file
-        new_pdf_index += 1
-        # add the `n` page to the `new_pdf_index` file
-        new_pdf_files[new_pdf_index].pages.append(page)
-        print(f"[*] Assigning Page {n} to the file {unit[new_pdf_index]}")
+    # iterate over all PDF pages
+    for n, page in enumerate(pdf.pages):
+        if n in list(range(*file2pages[new_pdf_index])):
+            # add the `n` page to the `new_pdf_index` file
+            new_pdf_files[new_pdf_index].pages.append(page)
+            print(f"[*] Assigning Page {n} to the file {unit[new_pdf_index]}")
+        else:
+            # make a unique filename based on original file name plus the index
+            name, ext = os.path.splitext(read_file)
+            names = name.split("/")
+            names.pop()
+            name = "/".join(names)
+            output_filename = f"{name}/{unit[new_pdf_index]}.pdf"
 
-# save the last PDF file
-name, ext = os.path.splitext(filename)
-names = name.split("/")
-names.pop()
-name = "/".join(names)
-output_filename = f"{name}/{unit[new_pdf_index]}.pdf"
-new_pdf_files[new_pdf_index].save(output_filename)
-print(f"[+] File: {output_filename} saved.")
+            # save the PDF file
+            new_pdf_files[new_pdf_index].save(output_filename)
+            print(f"[+] File: {output_filename} saved.")
+            # go to the next file
+            new_pdf_index += 1
+            # add the `n` page to the `new_pdf_index` file
+            new_pdf_files[new_pdf_index].pages.append(page)
+            print(f"[*] Assigning Page {n} to the file {unit[new_pdf_index]}")
+
+    # save the last PDF file
+    name, ext = os.path.splitext(read_file)
+    names = name.split("/")
+    names.pop()
+    name = "/".join(names)
+    output_filename = f"{name}/{unit[new_pdf_index]}.pdf"
+    new_pdf_files[new_pdf_index].save(output_filename)
+    print(f"[+] File: {output_filename} saved.")
